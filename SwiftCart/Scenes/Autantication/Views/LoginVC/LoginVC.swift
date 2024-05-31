42078fb8b3c5025dbe113e048a94e1a21b13e452
anas <hello.anas07@gmail.com>
2024-05-31 21:17:45 +0300

    Add design for SignUp screen in nib file

---
+++ SwiftCart/Scenes/Autantication/Views/LoginVC/LoginVC.swift
@@ -0,0 +1,32 @@
+//
+//  LoginVC.swift
+//  SwiftCart
+//
+//  Created by Anas Salah on 31/05/2024.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
