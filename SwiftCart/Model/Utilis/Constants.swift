7381350b548114dc0e5229712f5f7cb5cbe5a85b
Israa <isramuhammad613@gmail.com>
2024-06-11 10:21:59 -0700

    update Settings Coordinator with VC Identifiers

--- SwiftCart/Model/Utilis/Constants.swift
+++ SwiftCart/Model/Utilis/Constants.swift
@@ -23,12 +23,22 @@ public enum K {
         static let Product_View_Name = "ProductViewController"
     }
     
+    enum Settings {
+        static let Settings_Storyboard_Name = "SettingsStoryboard"
+        static let Settings_View_Name = "SettingsViewController"
+        static let Addresses_View_Name = "AddressesViewController"
+        static let Address_Details_View_Name = "AddressDetailsViewController"
+        static let Contact_Us_View_Name = "ContactUsViewController"
+    }
+    
     enum Shopify {
         static let Shopify_API_Key = "236f00d0acd3538f6713fd3a323150b6"
         static let Shopify_API_Secret = "f7e30cfe8bb4caedc97901323c1a2d67"
         static let Shopify_Shop_Name = "mad44-sv-iost1"
         static let Access_Token = "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"
-        static let Base_URL = "https://mad44-sv-iost1.myshopify.com/admin/api/2023-01/"
+        static let Base_URL = "https://mad44-sv-iost1.myshopify.com/admin/api/2024-04/"
+        static var userID =  UserDefaultsHelper.shared.getUserData().shopifyCustomerID ?? "6930899599407"
+
     }
     
     enum HTTPMethod {
@@ -44,6 +54,11 @@ public enum K {
         static let WOMEN = 287822348335
         static let KID = 287822381103
         static let SALE = 287822413871
+    }
+    
+    enum endPoints : String{
+        case getOrPostAddress = "/customers/{customer_id}/addresses.json"
+        case putOrDeleteAddress = "/customers/{customer_id}/addresses/{address_id}.json"
     }
     
     //MARK: Add any Const ants u need in a staruct pleas
