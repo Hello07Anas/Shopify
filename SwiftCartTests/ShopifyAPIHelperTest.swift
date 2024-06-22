b0b8df44f407bf564fedb9eb1f343df8701bb30e
anas <hello.anas07@gmail.com>
2024-06-23 00:09:00 +0300

    added one more testCase on cant make redunednt acc

--- SwiftCartTests/ShopifyAPIHelperTest.swift
+++ SwiftCartTests/ShopifyAPIHelperTest.swift
@@ -10,15 +10,15 @@ import XCTest
 import Alamofire
 
 final class ShopifyAPIHelperTest: XCTestCase {
-
+    
     override func setUpWithError() throws { // TODO: this will run before all tests func.
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws { // TODO: this will run after all tests func.
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -26,36 +26,36 @@ final class ShopifyAPIHelperTest: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
+    
     func testPerformanceExample() throws {
-    // This is an example of a performance test case.
+        // This is an example of a performance test case.
         //self.measure {
         //    // Put the code you want to measure the time of here.
         //}
     }
     
     
-//    func testCreateingCustomer() {
-//        let expectation = self.expectation(description: "Waiting for create customer API response")
-//                // TODO: have to change email with every run to pass this test
-//        ShopifyAPIHelper.shared.createCustomer(email: "test12@gmail.com", firstName: "anas", lastName: "salah") { result in
-//            switch result {
-//            case .success(let successMessage):
-//                print(successMessage)
-//                XCTAssert(successMessage.contains("Customer created with ID"), "Customer creation should return a success message with ID")
-//            case .failure(let error):
-//                XCTFail("Error creating customer: \(error.localizedDescription)")
-//            }
-//
-//            expectation.fulfill()
-//        }
-//
-//        waitForExpectations(timeout: 20, handler: nil)
-//    }
-
+    //    func testCreateingCustomer() {
+    //        let expectation = self.expectation(description: "Waiting for create customer API response")
+    //                // TODO: have to change email with every run to pass this test
+    //        ShopifyAPIHelper.shared.createCustomer(email: "test12@gmail.com", firstName: "anas", lastName: "salah") { result in
+    //            switch result {
+    //            case .success(let successMessage):
+    //                print(successMessage)
+    //                XCTAssert(successMessage.contains("Customer created with ID"), "Customer creation should return a success message with ID")
+    //            case .failure(let error):
+    //                XCTFail("Error creating customer: \(error.localizedDescription)")
+    //            }
+    //
+    //            expectation.fulfill()
+    //        }
+    //
+    //        waitForExpectations(timeout: 20, handler: nil)
+    //    }
+    
     func testCreateingDraftOrder() {
         let expectation = self.expectation(description: "Waiting for create draft order API response")
-
+        
         ShopifyAPIHelper.shared.createCustomer(email: "12test@gmail.com", firstName: "anas", lastName: "salah") { customerResult in
             switch customerResult {
             case .success(let successMessage):
@@ -65,7 +65,7 @@ final class ShopifyAPIHelperTest: XCTestCase {
                     expectation.fulfill()
                     return
                 }
-
+                
                 let lineItems: [[String: Any]] = [
                     [
                         "title": "Test Product",
@@ -73,7 +73,7 @@ final class ShopifyAPIHelperTest: XCTestCase {
                         "quantity": 1
                     ]
                 ]
-
+                
                 ShopifyAPIHelper.shared.createDraftOrder(customerId: String(customerId), lineItems: lineItems) { draftOrderResult in
                     switch draftOrderResult {
                     case .success(let draftOrderId):
@@ -92,17 +92,17 @@ final class ShopifyAPIHelperTest: XCTestCase {
                         } else {
                             print("General Error: \(error.localizedDescription)")
                         }
-
+                        
                         if let statusCode = (error as? AFError)?.responseCode, statusCode == 422 {
                             print("Handle specific validation error for status code 422")
                         }
-
+                        
                         XCTFail("Error creating draft order: \(error.localizedDescription)")
                     }
-
+                    
                     expectation.fulfill()
                 }
-
+                
             case .failure(let error):
                 print(error.localizedDescription)
                 if let afError = error.asAFError {
@@ -117,16 +117,39 @@ final class ShopifyAPIHelperTest: XCTestCase {
                 } else {
                     print("General Error: \(error.localizedDescription)")
                 }
-
+                
                 if let statusCode = (error as? AFError)?.responseCode, statusCode == 422 {
                     print("Handle specific validation error for status code 422")
                 }
                 XCTFail("Error creating draft order: \(error.localizedDescription)")
                 expectation.fulfill()
-                XCTFail("Error creating draft order: \(error.localizedDescription)")
-
             }
         }
         waitForExpectations(timeout: 20, handler: nil)
     }
+    
+    func testFaildToCreateRedundentDraftOrder() {
+        
+        let expectation = self.expectation(description: "Waiting for create draft order API response")
+        
+        
+        ShopifyAPIHelper.shared.createDraftOrder(customerId: String(7106376597551), lineItems: []) { draftOrderResult in
+            switch draftOrderResult {
+            case .success(let draftOrderId):
+                print("Draft order created with ID: \(draftOrderId)")
+                XCTFail("Error creating draft redundent order: \(draftOrderId)")
+            case .failure(let error):
+                print("In failure")
+                print(error.localizedDescription)
+                if let statusCode = (error as? AFError)?.responseCode {
+                    print("Handle specific validation error for status code \(statusCode)")
+                    
+                    XCTAssertEqual(statusCode, 422)
+                    expectation.fulfill()
+                }
+            }
+        }
+        self.waitForExpectations(timeout: 20, handler: nil)
+    }
 }
+    
